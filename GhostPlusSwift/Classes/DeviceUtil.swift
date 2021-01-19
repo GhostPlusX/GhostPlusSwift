578a19204fcb9a4e345f6c20712023758619aeea
DAVID IL YONG CHUN <daivd1000@gmail.com>
2021-01-19 12:57:14 +0900

    first commit

---
+++ GhostPlusSwift/Classes/DeviceUtil.swift
@@ -0,0 +1,20 @@
+//
+//  DeviceManager.swift
+//  GhostPlusSwift
+//
+//  Created by DAVID IL YONG CHUN on 2021/01/19.
+//
+
+import Foundation
+import DeviceCheck
+
+public class DeviceManager {
+    
+    static let sharedInstance:DeviceManager = DeviceManager() ;
+    
+    init() {
+        
+    }
+    
+    
+}
---
+++ Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  GhostPlusSwift
+//
+//  Created by DAVID IL YONG CHUN on 2021/01/19.
+//
+
+import Foundation
